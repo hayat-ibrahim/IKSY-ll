5b41f0a79e999bcba5a26e3aa5585a95a3588790
hayat-ibrahim <hayat.ibrahim@stud.hs-bochum.de>
2025-04-21 14:43:05 +0200

    composer config

---
+++ Kino/vendor/composer/installed.php
@@ -0,0 +1,38 @@
+<?php return array(
+    'root' => array(
+        'name' => 'iksy/kino',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '37557e01f1dbbfc3f11ddc9f3020c7ad8fabfa1f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'iksy/kino' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '37557e01f1dbbfc3f11ddc9f3020c7ad8fabfa1f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twbs/bootstrap' => array(
+            'pretty_version' => 'v5.3.5',
+            'version' => '5.3.5.0',
+            'reference' => '85f23534bd2de8041354b297516cf21959091b31',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twitter/bootstrap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => 'v5.3.5',
+            ),
+        ),
+    ),
+);
